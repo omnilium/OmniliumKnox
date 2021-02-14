6b2628615df0ab81b52c09193ab96c612dc41490
Andrei Hava <andrei.hava@eminus-software.com>
2021-02-14 19:02:04 +0200

    Fixed Sonar build

--- sonar-build.ps1
+++ sonar-build.ps1
@@ -7,7 +7,7 @@ $SONAR_ORGANIZATION="omnilium"
 $SONAR_URL = If ( $SONAR_URL ) { $SONAR_URL } else {"https://sonarcloud.io"}
 
 mkdir $HOME/.sonar
-$SONAR_SCANNER_VERSION = "4.2.0.1873"
+$SONAR_SCANNER_VERSION = "4.5.0.2216"
 $SONAR_SCANNER_HOME = "$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-windows"
 
 $path = "$HOME/.sonar/build-wrapper-win-x86.zip"
@@ -19,7 +19,7 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory($path, "$HOME/.sonar")
 $env:Path += ";$HOME/.sonar/build-wrapper-win-x86"
 
-$path = "$HOME/.sonar/sonar-scanner-cli-4.2.0.1873-windows.zip"
+$path = "$HOME/.sonar/sonar-scanner-cli-$SONAR_SCANNER_VERSION-windows.zip"
 rm sonar-scanner -Recurse -Force -ErrorAction SilentlyContinue
 rm $path -Force -ErrorAction SilentlyContinue
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -34,8 +34,4 @@ cmake .. -DCMAKE_BUILD_TYPE:STRING="Release" -DCMAKE_INSTALL_PREFIX:PATH="artifa
 
 build-wrapper-win-x86-64 --out-dir sonar cmake --build . --config Release
 
-$SONAR_TOKEN_CMD_ARG = If ( $SONAR_TOKEN ) { "-D sonar.login=$SONAR_TOKEN" }
-$SONAR_ORGANIZATION_CMD_ARG = If ( $SONAR_ORGANIZATION ) { "-D sonar.organization=$SONAR_ORGANIZATION" }
-$SONAR_PROJECT_NAME_CMD_ARG = If ( $SONAR_PROJECT_NAME ) { "-D sonar.projectName=$SONAR_PROJECT_NAME" }
-$SONAR_OTHER_ARGS = @("-D sonar.projectVersion=0.1","-D sonar.cfamily.build-wrapper-output=sonar","-D sonar.sourceEncoding=UTF-8")
-sonar-scanner.bat -D sonar.host.url=$SONAR_URL -D sonar.projectKey=$SONAR_PROJECT_KEY $SONAR_OTHER_ARGS $SONAR_PROJECT_NAME_CMD_ARG $SONAR_TOKEN_CMD_ARG $SONAR_ORGANIZATION_CMD_ARG
+sonar-scanner -D sonar.host.url=$SONAR_URL -D sonar.projectKey=${SONAR_ORGANIZATION}:${SONAR_PROJECT_KEY} -D sonar.organization=$SONAR_ORGANIZATION -D sonar.projectName=OmniliumKnox -D sonar.projectVersion=$BUILD_VERSION -D sonar.sourceEncoding=UTF-8
