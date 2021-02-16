04d9370588796504418f9e50d59939c21f7fbd83
Andrei Hava <andrei.hava@eminus-software.com>
2021-02-16 14:22:52 +0200

    Fixed CI build

--- ci-build.ps1
+++ ci-build.ps1
@@ -3,6 +3,6 @@ Set-Location ./ci-build/
 cmake .. -DCMAKE_BUILD_TYPE:STRING="Release"
 cmake --build . --config Release
 New-Item -ItemType directory -Path ./artifacts
-Copy-Item -Path ./Core/Release/* -Destination ./artifacts -PassThru -Recurse -Force -Confirm:$False
-Copy-Item -Path ./KnoxC/Release/* -Destination ./artifacts -PassThru -Recurse -Force -Confirm:$False
-Copy-Item -Path ./Knox/Release/* -Destination ./artifacts -PassThru -Recurse -Force -Confirm:$False
+Copy-Item -Path ./core/Release/* -Destination ./artifacts -PassThru -Recurse -Force -Confirm:$False
+Copy-Item -Path ./knoxcli/Release/* -Destination ./artifacts -PassThru -Recurse -Force -Confirm:$False
+Copy-Item -Path ./knox/Release/* -Destination ./artifacts -PassThru -Recurse -Force -Confirm:$False
