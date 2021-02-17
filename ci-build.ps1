a89a6bb55104f132e9a57c4e2d5cb817fad5406a
Andrei Hava <hava.ionut@gmail.com>
2021-02-17 15:48:26 +0200

    AB#13 - Fixed CI script to exclude *.exp and *.lib files

--- ci-build.ps1
+++ ci-build.ps1
@@ -3,6 +3,8 @@ Set-Location ./ci-build/
 cmake .. -DCMAKE_BUILD_TYPE:STRING="Release"
 cmake --build . --config Release
 New-Item -ItemType directory -Path ./artifacts
-Copy-Item -Path ./core/Release/* -Destination ./artifacts -PassThru -Recurse -Force -Confirm:$False
-Copy-Item -Path ./knoxcli/Release/* -Destination ./artifacts -PassThru -Recurse -Force -Confirm:$False
-Copy-Item -Path ./knox/Release/* -Destination ./artifacts -PassThru -Recurse -Force -Confirm:$False
+
+$Exclude = @("*.exp", "*.lib")
+Copy-Item -Path ./core/Release/* -Destination ./artifacts -PassThru -Recurse -Force -Confirm:$False -Exclude $Exclude
+Copy-Item -Path ./knoxcli/Release/* -Destination ./artifacts -PassThru -Recurse -Force -Confirm:$False -Exclude $Exclude
+Copy-Item -Path ./knox/Release/* -Destination ./artifacts -PassThru -Recurse -Force -Confirm:$False -Exclude $Exclude
